2970742f3fec09650c78e5b67c59fec37b1a88be
Stiven <codestiven@gmail.com>
2024-01-08 11:16:56 -0400

    5. Componentes

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import UsingRefComponent from './components/UsingRefComponent';
 import UseState from './components/UseState';
+import Mycomponent from './components/mycomponents';
 
 function App() {
   return (
     <div className="App">
       <UsingRefComponent />
       <UseState />
+      <Mycomponent />
     </div>
   );
 }
---
+++ src/components/mycomponents/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+
+
+const Mycomponent = () => {
+    return (
+        <div>
+            <h1>My component</h1>
+        </div>
+    )
+}
+
+export default Mycomponent;
